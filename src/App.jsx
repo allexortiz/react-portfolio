29887d7fd6ec48d61a4800527145e642903a305b
Allex Ortiz <allexortiz@outlook.com>
2024-02-06 14:44:00 -0600

    added background image to all pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { Outlet } from 'react-router-dom';
 import NavTabs from './components/NavTabs';
 import Footer from './components/footer';
 import './app.css'
+import backgroundImage from './assets/background.png';
 
 function App() {
   return (
-    <section className='container-custom'>
+    <section className='container-custom' style={{backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', minHeight: '100vh'}}>
       <NavTabs />
       <main className="mx-3 main">
         <Outlet />
