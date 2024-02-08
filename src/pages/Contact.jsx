5d20732ac7cc7e246d7f6c55d09d06b37dd080e8
Allex Ortiz <allexortiz@outlook.com>
2024-02-07 22:47:22 -0600

    uploaded resume.pdf, stylized resume tab, and added downloadable resume link

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -21,4 +21,4 @@ export default function Contact() {
       </section>
     </div>
   );
-};
+};5
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,7 +1,61 @@
+import React from "react";
+import myResume from "../assets/myResume.pdf";
+import { BsCloudDownloadFill } from "react-icons/bs";
 export default function Resume() {
-    return (
-      <div>
-        <h1>Resume</h1>
+  return (
+    <div>
+      <section>
+        <div className="d-flex justify-content-center">
+          <h1>My Resume<a href={myResume} download><BsCloudDownloadFill size={30} /></a></h1>
+        </div>
+      </section>
+      <div className="container vh-100 d-flex justify-content-center align-items-center flex-column">
+        <h2 className="mb-5">Skills I learned in full-stack coding bootcamp:</h2>
+        <section className="row gx-4 justify-content-center">
+          <div className="col">
+            <div className="d-flex flex-column">
+              <h3>Font-End Tech</h3>
+              <ul className="fs-4">
+                <li>HTML5</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>APIs</li>
+                <li>Bootstrap</li>
+                <li>GIT</li>
+              </ul>
+            </div>
+          </div>
+          <div className="col">
+            <div className="d-flex flex-column">
+              <h3>Back-End Tech</h3>
+              <ul className="fs-4">
+                <li>Node.Js</li>
+                <li>Jest</li>
+                <li>Express.js</li>
+                <li>MySQL</li>
+                <li>Sequelize</li>
+                <li>Object-Relational Mapping(ORM)</li>
+                <li>Model View Controller(MVC)</li>
+              </ul>
+            </div>
+          </div>
+          <div className="col">
+            <div className="d-flex flex-column">
+              <h3>Performance Tech</h3>
+              <ul className="fs-4">
+                <li>NoSQL</li>
+                <li>Progressive Web Applications (PWA)</li>
+                <li>MongoDB</li>
+                <li>Mongoose</li>
+                <li>GraphQL</li>
+                <li>React</li>
+                <li>MERN</li>
+                <li>State</li>
+              </ul>
+            </div>
+          </div>
+        </section>
       </div>
-    );
-  }
+    </div>
+  );
+}
